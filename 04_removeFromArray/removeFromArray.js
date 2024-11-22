32e7496077a0b7f20acac3f71f89cb676ef8de65
Pavel Nam <115383491+nampavel@users.noreply.github.com>
2024-11-21 21:50:09 -0500

    feat: add solution to exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, removingNumber) {
+    let args = Array.from(removingNumber);
+    let lArgs = args.length;
+    let indexOfRemovingNumber = myArray.indexOf(args);
+    myArray.splice(indexOfRemovingNumber, lArgs);
+    return args;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startingNum, endingNum ) {
+    let finalSum = 0;
+    let updatedStartingNum = 0;
+    let updatedEndingNum = 0;
 
+    
+    if (startingNum > endingNum) {
+        updatedStartingNum = endingNum;
+        updatedEndingNum = startingNum;
+        startingNum = updatedStartingNum;
+        endingNum = updatedEndingNum;
+    } 
+    else if (startingNum < 0 || endingNum < 0) {
+        return finalSum = "ERROR";
+    } 
+    else if (!Number.isInteger(startingNum) || !Number.isInteger(endingNum)) {
+        return finalSum = "ERROR";
+    } 
+    while (startingNum > 0 && startingNum <= endingNum) {
+        finalSum = finalSum + startingNum;
+        startingNum++;
+    }
+    return finalSum;
+ 
 };
 
 // Do not edit below this line
